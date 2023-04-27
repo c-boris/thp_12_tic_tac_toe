002d4f1eb2d01d7f3bbe3f42e1bf5e187e85dcc2
cboris <b.crochet@gmail.com>
2023-04-27 23:38:10 +0200

    add files

--- lib/show.rb
+++ lib/show.rb
@@ -1,11 +1,11 @@
 class Show
   def show_board(board)
     system "clear"
-    puts " #{board.cases[1].value} | #{board.cases[2].value} | #{board.cases[3].value} "
+    puts " #{board.cases[1].value.colorize(:light_yellow)} | #{board.cases[2].value.colorize(:light_yellow)} | #{board.cases[3].value.colorize(:light_yellow)} "
     puts "---+---+---"
-    puts " #{board.cases[4].value} | #{board.cases[5].value} | #{board.cases[6].value} "
+    puts " #{board.cases[4].value.colorize(:light_yellow)} | #{board.cases[5].value.colorize(:light_yellow)} | #{board.cases[6].value.colorize(:light_yellow)} "
     puts "---+---+---"
-    puts " #{board.cases[7].value} | #{board.cases[8].value} | #{board.cases[9].value} "
+    puts " #{board.cases[7].value.colorize(:light_yellow)} | #{board.cases[8].value.colorize(:light_yellow)} | #{board.cases[9].value.colorize(:light_yellow)} "
     puts " "
   end
 end
